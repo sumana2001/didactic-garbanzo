58d5e90ca3f770d48db527447ddf440ff15dccdc
sumana2001 <sumana.basu2001@gmail.com>
2021-02-05 17:42:47 +0530

    Create

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-app=Flask(__name__)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/',methods=['POST','GET'])
 def index():
-    return ('<h1>heyo</h1>')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content = task_content)
 
-if __name__=='__main__':
-    app.run(debug='True')
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        
+        except:
+            return 'Error adding new task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+if __name__ == '__main__':
+    app.run(debug = 'True')
